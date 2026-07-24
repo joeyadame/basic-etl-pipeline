2ad6c371411fca78ff7cba8e2ba88e9c0d1e08df
Janelle Adame <janelleadame@Mac.lan>
2026-07-23 20:53:47 -0500

    enhanced validation contracts

--- src/main.py
+++ src/main.py
@@ -8,16 +8,11 @@ from validate import validate_record_count, validate_transformation_preload
 
 metadata_total_records, paginated_response = extract_api_response(url)
 
-if validate_record_count(metadata_total_records, paginated_response):
-    engine = create_engine('postgresql+psycopg2://postgres@localhost:5432/eia_energy')
-    df = transform_dataframe(paginated_response)
-    if validate_transformation_preload(df, validate_record_count(metadata_total_records, paginated_response)):
-        load_dataframe(df, engine)
-    else:
-        print('data transformation verification failed')
-else:
-    print('metadata verification failed, expected ', metadata_total_records, 
-        ' request returned ', len(paginated_response), ' records')
+true_record_count = validate_record_count(metadata_total_records, paginated_response)
+engine = create_engine('postgresql+psycopg2://postgres@localhost:5432/eia_energy')
+df = transform_dataframe(paginated_response)
+validate_transformation_preload(df, true_record_count)
+load_dataframe(df, engine)
 
 
 
--- src/validate.py
+++ src/validate.py
@@ -1,15 +1,14 @@
 # =====================
 # VALIDATE (functioned)
 # =====================
+import sys
 
 def validate_record_count(metadata_total_records, paginated_response):
     if(len(paginated_response) == int(metadata_total_records)):
         return len(paginated_response)
-    else:
-        return False
+    sys.exit("metatata record count validation failed, terminating extraction execution")
 
 def validate_transformation_preload(df, verified_record_count):
     if(len(df) == verified_record_count):
         return True
-    else: 
-        return False
+    sys.exit("preload record count validation failed, terminating extraction execution")
