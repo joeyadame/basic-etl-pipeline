9d7721d0c1337449e0cd4290e9e86420a79b3278
joeyadame <121279018+joeyadame@users.noreply.github.com>
2026-07-24 22:37:59 -0500

    Fix syntax error in RowCountValidationError class

--- src/exceptions.py
+++ src/exceptions.py
@@ -4,6 +4,6 @@ class DatabaseConnectionError(Exception):
 	#Exception for failed DB conection
 	pass
 
-class RowCountValidationError(Exception)
+class RowCountValidationError(Exception):
 	#Exception for row count discrepencies between pipeline stages
 	pass
