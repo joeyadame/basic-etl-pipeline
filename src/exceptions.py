8a95414e9e789d85efcc0306e535a78afd95aded
Janelle Adame <janelleadame@Mac.lan>
2026-07-26 09:57:53 -0500

    deleted uneccesary custom exceptions file

--- src/exceptions.py
+++
@@ -1,9 +0,0 @@
-# exceptions.py
-
-class DatabaseConnectionError(Exception):
-	#Exception for failed DB conection
-	pass
-
-class RowCountValidationError(Exception):
-	#Exception for row count discrepencies between pipeline stages
-	pass
